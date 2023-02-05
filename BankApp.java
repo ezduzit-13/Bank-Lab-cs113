fd941ef3dadd48933f924f81bd0b6926bbdd4759
ezduzit-13 <63536876+ezduzit-13@users.noreply.github.com>
2023-02-04 19:01:40 -0800

    professor said it was done

--- BankApp.java
+++ BankApp.java
@@ -3,12 +3,11 @@
  * !Array Index out of bounds exception
  * A0000000001
  */
-
 public class BankApp {
   public static void main(String[] args) {
     Bank bank = new Bank();
     BankGUI gui = new BankGUI();
     gui.processCommands(bank);
-  }
-  
+  }  
 }
+ 
